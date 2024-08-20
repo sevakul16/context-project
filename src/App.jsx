b71f00bc4262594e771563ce75b25413ea83ec8a
sevakul16 <sevakul16@gmail.com>
2024-08-20 12:16:52 +0300

    Small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import CardContextProvider from "./store/shopping-cart.jsx";
 
 function App() {
   return (
-    <CardContextProvider v>
+    <CardContextProvider>
       <Header />
       <Shop />
     </CardContextProvider>
